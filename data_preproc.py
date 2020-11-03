0588d6872fe85f62e5ce9ed8b3a1fee6247cc13d
K1ngAng31 <angelsantanahernandez2@gmail.com>
2020-11-03 15:04:34 -0700

    CSV files

--- data_preproc.py
+++ data_preproc.py
@@ -4,25 +4,68 @@ import pandas as pd
 # Work for news_sample.csv
 
 df_news_sample = pd.read_csv('news_sample.csv')#read in the news sample csv
-print(df_news_sample.head())
+# print(df_news_sample.head())
 
-'''This is the cleaned data, took off as many NaN columns, but their is alot, each column and row is filled with some, so i took out the summary and tags
-and left the other ones.'''
+
 #create new df
 #took off some of the the nan vcolumns
-dfnewssample = df_news_sample[['id','domain','type','url','content','scraped_at','inserted_at','updated_at','title','authors','keywords','meta_keywords','meta_description']].copy()
+# id, url, content, title, keywords, tags, source
+dfnewssample = df_news_sample[['id','url','content','title','keywords','tags']].copy()
+dfnewssample.to_csv(r'news_sample_clean.csv', header=True)
 print(dfnewssample.head(100))
 print("===============================")
 
 
 # Work for websites.csv
-#read in the website csv
-df_websites = pd.read_csv('websites.csv')
+df_websites = pd.read_csv('websites.csv')#read in the website csv
 print(df_websites.head())
 print('')
 
-'''This is the cleaned data, took off the NaN values, now it will just show the url and type'''
-#create new df
-#took off some of the the nan vcolumns
-dfwebsites = df_websites[['url','type']].copy()
-print(dfwebsites.head(1000))
+#First type
+df_type = df_websites[['url','type']].copy()#create new df
+print(df_type.head())# this will print all data in the csv file with all types
+df_type.to_csv(r'wtype_clean.csv', header=True)
+print('')
+
+
+df_type_fake = df_type[df_type['type'] == 'fake']# this will print only data with first type = fake
+print(df_type_fake.head())
+df_type_fake.to_csv(r'wtype_only_fake_news.csv', header=True)
+print('')
+# this prints everything but fake news in first type
+df_type_notfake = df_type[df_type['type'] != 'fake']
+df_type_notfake.to_csv(r'wnot_fake_news.csv', header=True)
+print(df_type_notfake.head())
+
+#Second type
+df_2nd_type = df_websites[['url','2nd_type']].copy()
+
+print(df_2nd_type.head())
+
+print('')
+# this will print only data with second type = fake
+df_2nd_type_fake = df_2nd_type[df_2nd_type['2nd_type'] == 'fake']
+
+print(df_2nd_type_fake.head())
+df_2nd_type_fake.to_csv(r'w2nd_type_isfake_news.csv', header=True)
+
+print('')
+# this prints everything but fake news in second type
+df_2nd_type_notfake = df_2nd_type[df_2nd_type['2nd_type'] != 'fake']
+df_2nd_type_notfake.to_csv(r'w2nd_type_not_fake_news.csv', header=True)
+print(df_2nd_type_notfake.head())
+
+print('')
+df_3rd_type = df_websites[['url','3rd_type']].copy()
+print(df_3rd_type.head())
+print('')
+# this will print only data with second type = fake
+df_3rd_type_fake = df_3rd_type[df_3rd_type['3rd_type'] == 'fake']
+
+print(df_3rd_type_fake.head())
+df_3rd_type_fake.to_csv(r'w3rd_type_is_fake_news.csv', header=True)
+print('')
+# this prints everything but fake news in second type
+df_3rd_type_notfake = df_3rd_type[df_3rd_type['3rd_type'] != 'fake']
+df_3rd_type_notfake.to_csv(r'w3rd_type_not_fake_news.csv', header=True)
+print(df_3rd_type_notfake.head())
